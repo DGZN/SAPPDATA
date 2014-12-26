c20c55591218b1ef404169dbee823c809a4873f6
Keiichi Lindley <digizn@gmail.com>
2014-12-26 15:35:55 -0700

    Adding Production Environment Configuration

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
     'development' => 'localhost',
-    'production'  => return $_SERVER['LARAVEL_ENV'];
+    'production'  => $_SERVER['LARAVEL_ENV']
 ));
 
 /*
